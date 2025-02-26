e697a1ed0545ce276e10327efa4369f89a82eabd
Max Netyaga <max.netyaga@gmail.com>
2025-02-26 16:01:33 +0200

    edit check

--- check.sh
+++ check.sh
@@ -1,3 +1,3 @@
 go run rmazur.io/p1-commits-check@latest \
     -commit $(git rev-parse HEAD) -deadline 2025-02-28 \
-    https://github.com/lady-and-the-beasts/software-architecture-lab1
+    https://github.com/maxnetyaga/software-architecture-lab1
